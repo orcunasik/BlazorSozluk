c4fc1bf712bff9af5c757bc291753c66d5c85acd
Orçun Aşık <asikorcun@gmail.com>
2023-01-21 17:59:05 +0300

    A static extension method has been created to add services.

---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Extensions/Registration.cs
@@ -0,0 +1,26 @@
+﻿using BlazorSozluk.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorSozluk.Infrastructure.Persistence.Extensions
+{
+    public static class Registration
+    {
+        public static IServiceCollection AddInfrastructureRegistration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BlazorSozlukContext>(conf =>
+            {
+                var connStr = configuration["SqlServerConnectionString"].ToString();
+                conf.UseSqlServer(connStr, opt =>
+                {
+                    opt.EnableRetryOnFailure();
+                });
+            });
+
+            //var seedData = new SeedData();
+            //seedData.SeedAsync(configuration).GetAwaiter().GetResult();
+            return services;
+        }
+    }
+}
