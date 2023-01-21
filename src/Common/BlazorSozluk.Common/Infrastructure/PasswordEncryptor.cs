813acd881ab8eb99f3a6d1b428ec66ceac0990e1
Orçun Aşık <asikorcun@gmail.com>
2023-01-21 18:07:29 +0300

    A static method has been added for the password to be created for the User model in the SeedData class.

---
+++ src/Common/BlazorSozluk.Common/Infrastructure/PasswordEncryptor.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BlazorSozluk.Common.Infrastructure
+{
+    public class PasswordEncryptor
+    {
+        public static string Encrypt(string password)
+        {
+            using var md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(password);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            return Convert.ToHexString(hashBytes);
+        }
+    }
+}
