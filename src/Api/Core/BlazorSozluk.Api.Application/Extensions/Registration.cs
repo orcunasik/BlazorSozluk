f034bb3055b3081abf9b2c69f4cabe92138fb4bd
Orçun Aşık <asikorcun@gmail.com>
2023-04-12 23:51:53 +0300

    The Registration class we manage Services has been created for Program.cs

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Extensions/Registration.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using MediatR;
+
+namespace BlazorSozluk.Api.Application.Extensions
+{
+    public static class Registration
+    {
+        public static IServiceCollection AddApplicationRegistration(this IServiceCollection services)
+        {
+            var assm = Assembly.GetExecutingAssembly();
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assm));
+            services.AddAutoMapper(assm);
+            services.AddValidatorsFromAssembly(assm);
+            return services;
+        }
+    }
+}
