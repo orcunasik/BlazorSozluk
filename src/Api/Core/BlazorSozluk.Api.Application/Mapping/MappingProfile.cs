5e96c52c2d04e4763c539a7faffee55394618cb4
Orçun Aşık <asikorcun@gmail.com>
2023-04-13 00:00:38 +0300

    ViewModel and MappingProfile classes were created for User Login process.

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BlazorSozluk.Api.Domain.Models;
+using BlazorSozluk.Common.Models.Queries;
+
+namespace BlazorSozluk.Api.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, LoginUserViewModel>().ReverseMap();
+        }
+    }
+}
