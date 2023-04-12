4599e096e974d7ff2ffe237d1d43d3f77f00e1dc
Orçun Aşık <asikorcun@gmail.com>
2023-04-12 23:58:54 +0300

    UserCommand,CommandHandler and CommandValidator and UserController classes were created for User Login process.

---
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using BlazorSozluk.Common.Models.RequestModels;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorSozluk.Api.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody]LoginUserCommand command)
+        {
+            var res = await _mediator.Send(command);
+            return Ok(res);
+        }
+    }
+}
---
+++ src/Common/BlazorSozluk.Common/Models/RequestModels/LoginUserCommand.cs
@@ -0,0 +1,21 @@
+﻿using BlazorSozluk.Common.Models.Queries;
+using MediatR;
+
+namespace BlazorSozluk.Common.Models.RequestModels
+{
+    public class LoginUserCommand : IRequest<LoginUserViewModel>
+    {
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public LoginUserCommand(string emailAddress, string password)
+        {
+            EmailAddress = emailAddress;
+            Password = password;
+        }
+
+        public LoginUserCommand()
+        {
+            
+        }
+    }
+}
