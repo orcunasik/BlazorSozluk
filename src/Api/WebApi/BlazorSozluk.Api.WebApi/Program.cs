4a503c226093245489c984eff955dbe5d5fa7d0b
Orçun Aşık <asikorcun@gmail.com>
2023-04-13 00:05:43 +0300

    Configured

--- src/Api/WebApi/BlazorSozluk.Api.WebApi/Program.cs
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Program.cs
@@ -1,15 +1,30 @@
+using BlazorSozluk.Api.Application.Extensions;
+using BlazorSozluk.Api.Application.Features.Commands.User;
 using BlazorSozluk.Infrastructure.Persistence.Extensions;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(opt =>
+    {
+        opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+    });
+
+builder.Services
+    .AddValidatorsFromAssembly(typeof(LoginUserCommandValidator).Assembly)
+    .AddFluentValidationAutoValidation();
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfrastructureRegistration(builder.Configuration);
+builder.Services.AddApplicationRegistration();
 
 var app = builder.Build();
 
