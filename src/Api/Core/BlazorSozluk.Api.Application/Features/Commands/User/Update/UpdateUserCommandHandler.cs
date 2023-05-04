36b3d555960df17c18b15b4f4b80ede35a785e92
Orçun Aşık <asikorcun@gmail.com>
2023-05-04 20:29:59 +0300

    Created UpdatedUserCommand ve Handler classes

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Features/Commands/User/Update/UpdateUserCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using BlazorSozluk.Api.Application.Interfaces.Repositories;
+using BlazorSozluk.Common.Events.User;
+using BlazorSozluk.Common.Infrastructure;
+using BlazorSozluk.Common;
+using BlazorSozluk.Common.Infrastructure.Exceptions;
+using BlazorSozluk.Common.Models.RequestModels;
+using MediatR;
+
+namespace BlazorSozluk.Api.Application.Features.Commands.User.Update
+{
+    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, Guid>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
+        public UpdateUserCommandHandler(IMapper mapper, IUserRepository userRepository)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+        }
+        public async Task<Guid> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var dbUser = await _userRepository.GetByIdAsync(request.Id);
+            if(dbUser is null)
+                throw new DatabaseValidationException("User Not Found!");
+            var dbEmailAddress = dbUser.EmailAddress;
+            var emailChanged = string.CompareOrdinal(dbEmailAddress, request.EmailAddress) != 0;
+            _mapper.Map(request, dbUser);
+
+            var rows = await _userRepository.UpdateAsync(dbUser);
+
+            //Check If email changed
+            if (rows > 0)
+            {
+                var @event = new UserEmailChangedEvent()
+                {
+                    OldEmailAddress = null,
+                    NewEmailAddress = dbUser.EmailAddress
+                };
+                QueueFactory.SendMessageToExchange(exchangeName: SozlukConstants.UserExchangeName,
+                                            exchangeType: SozlukConstants.DefaultExchangeType,
+                                            queueName: SozlukConstants.UserEmailChangedQueueName,
+                                            obj: @event);
+                dbUser.EmailConfirmed = false;
+                await _userRepository.UpdateAsync(dbUser);
+            }
+
+
+            return dbUser.Id;
+        }
+    }
+}
