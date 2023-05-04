7b0e5b4bb4caae250b88c76df1e8a52c4fd7c933
Orçun Aşık <asikorcun@gmail.com>
2023-05-04 20:43:55 +0300

    Added constant values ​​for use in the QueueFactory class.

---
+++ src/Common/BlazorSozluk.Common/SozlukConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorSozluk.Common
+{
+    public class SozlukConstants
+    {
+        public const string RabbitMQHost = "amqps://bmwjeecv:cYDbvuV3bLdtjR7iZw0rO61YFNpR-VxG@rat.rmq2.cloudamqp.com/bmwjeecv";
+        public const string DefaultExchangeType = "direct";
+
+        public const string UserExchangeName = "UserExchange";
+        public const string UserEmailChangedQueueName = "UserEmailChangedQueueName";
+    }
+}
