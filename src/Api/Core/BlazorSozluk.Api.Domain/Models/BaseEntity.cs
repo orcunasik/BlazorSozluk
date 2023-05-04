cdbc1e40e168d7c42f0b3c3e7cd6093a1d526886
Orçun Aşık <asikorcun@gmail.com>
2023-05-04 20:34:50 +0300

    Removed unused "usings". CreatedDate initialized.

--- src/Api/Core/BlazorSozluk.Api.Domain/Models/BaseEntity.cs
+++ src/Api/Core/BlazorSozluk.Api.Domain/Models/BaseEntity.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlazorSozluk.Api.Domain.Models
+﻿namespace BlazorSozluk.Api.Domain.Models
 {
     public abstract class BaseEntity
     {
         public Guid Id { get; set; }
-        public DateTime CreateDate { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
     }
 }
